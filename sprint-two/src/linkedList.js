da195fc36736cc5e630be2dabe2a1e96b40b9476
thejsj <jorge.silva@thejsj.com>
2014-10-29 16:09:51 -0700

    Starting on linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,18 +1,47 @@
 var makeLinkedList = function(){
-  var list = {};
+  var list = {}; // public
   list.head = null;
   list.tail = null;
+  list.values = {};
 
   list.addToTail = function(value){
+    list.values[value] = makeNode(value);
+    // Assign tail to previuos tail
+    if (list.tail !== null) {
+      list.tail.next = list.values[value];
+    }
+    // Re-assign tail
+    list.tail = list.values[value];
+
+    if (list.head === null) {
+      list.head = list.values[value];
+    }
+    console.log('addToTail: ' + value);
+    console.log(list.values);
   };
 
   list.removeHead = function(){
+    var temp = list.head;
+    list.head = list.head.next;
+    delete list.values[temp.value];
+    return temp.value;
   };
 
   list.contains = function(target){
+    console.log('contains');
+    console.log(target);
+    console.log(list);
+    if (target in list.values) {
+   //   debugger;
+      console.log(list.indexOf(target));
+      return list[target];
+    }
+    //debugger;
+    console.log('false');
+    return false;
   };
 
-  return list;
+  return list; // returned to the user
 };
 
 var makeNode = function(value){
@@ -23,6 +52,8 @@ var makeNode = function(value){
 
   return node;
 };
+
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
