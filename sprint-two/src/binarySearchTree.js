b1f5e7d043dae2a0cdd5adb676e96d3c720ea504
thejsj <jorge.silva@thejsj.com>
2014-10-30 16:10:24 -0700

    Adding binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,56 @@
 var makeBinarySearchTree = function(value){
+  var tree = {};
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  tree.insert = function(value) {
+    if (value === tree.value) {
+      return false;
+    }
+    if (value < tree.value) {
+      if (tree.left === null) {
+        tree.left = makeBinarySearchTree(value);
+        return tree.left;
+      } else {
+        return tree.left.insert(value);
+      }
+    } else {
+      if (tree.right === null) {
+        tree.right = makeBinarySearchTree(value);
+        return tree.right;
+      } else {
+        return tree.right.insert(value);
+      }
+    }
+  }
+  tree.contains = function (value) {
+    if (value === tree.value) {
+      return true;
+    }
+    if (value < tree.value) {
+      if (tree.left === null) {
+        return false;
+      } else {
+        return tree.left.contains(value);
+      }
+    } else {
+      if (tree.right === null) {
+        return false;
+      } else {
+        return tree.right.contains(value);
+      }
+    }
+  }
+  tree.depthFirstLog = function(func) {
+    func(tree.value);
+    if (tree.left !== null) {
+      tree.left.depthFirstLog(func);
+    }
+    if (tree.right !== null) {
+      tree.right.depthFirstLog(func);
+    }
+  }
+  return tree;
 };
 
 
