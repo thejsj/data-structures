4f154b420a4651e240c84876d7a896d3ac2e454a
thejsj <jorge.silva@thejsj.com>
2014-10-31 16:37:40 -0700

    Adding treeWithParent

---
+++ sprint-two/src/treeWithParent.js
@@ -0,0 +1,52 @@
+var makeTreeWithParent = function (value, parent) {
+  var newTree = {};
+  newTree.value = value;
+  newTree.children = [];
+  newTree.parent = parent || null;
+  extend(newTree, treeMethods);
+  return newTree;
+};
+
+var treeMethods = {};
+
+treeMethods.addChild = function (value) {
+  this.children.push(makeTreeWithParent(value, this));
+};
+
+treeMethods.contains = function (target) { // string
+  if (this.value === target) return true;
+  // Go through all values in the array and check wether they're present
+  for (var i = 0; i < this.children.length; i++) {
+    var child = this.children[i];
+    // If this is a tree...
+    if (child.contains(target)) return true;
+  }
+  return false;
+};
+
+treeMethods.removeChild = function (treeWithParent) {
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i] === treeWithParent) {
+      this.children.splice(i, 1);
+      return true;
+    }
+  }
+  return false;
+};
+
+treeMethods.removeFromParent = function () {
+  var remove_child = this.parent.removeChild(this);
+  this.parent = null;
+  return this;
+};
+
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
